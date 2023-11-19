4badbd14d416738c06b57fc60ea740725f7996ef
harshahossain <harshahossain@gmail.com>
2023-11-19 15:11:04 +0600

    Added questions

---
+++ src/questions.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    id: "q1",
+    text: "Who is the Youngest ballon d'or Winner?",
+    answers: [
+      "Ronaldo Nazario - Brazil.",
+      "Michael Owen - England",
+      "Lionel Messi - Argentina",
+      "George Best - Northern Ireland",
+    ],
+  },
+  {
+    id: "q2",
+    text: "Who has scored the most goals in football club history?",
+    answers: [
+      "Cristiano Ronaldo - Portugal",
+      "Lionel Messi- Argentina",
+      "Pele - Brazil",
+      "Romario -Brazil",
+    ],
+  },
+  {
+    id: "q3",
+    text: "What happens in when the result is 2-2 after 90 mins in a knockout round?",
+    answers: [
+      "The game proceeds to extra-time of 30 minutes after a break.",
+      "The game proceeds to penalty shootout.",
+      "The game proceeds for an unlimited amount of time till a team scores.",
+      "The match ends in draw and which team scored the first goal wins.",
+    ],
+  },
+  {
+    id: "q4",
+    text: "Which team here has never won Champions league and Europa league?",
+    answers: [
+      "Arsenal - Premier League.",
+      "Borussia Dortmund - Bundesliga.",
+      "Valencia - La Liga.",
+      "Parma - Seria A.",
+    ],
+  },
+  {
+    id: "q5",
+    text: "Who is the all time leading goal s Cup?",
+    answers: [
+      "Miroslav Kolse - Germany.",
+      "Ronaldo Nazario - Brazil.",
+      "Lionel Messi - Argentina.",
+      "Just Fontaine - France.",
+    ],
+  },
+  {
+    id: "q6",
+    text: "What is a linesman?",
+    answers: [
+      "The referee's assistant",
+      "The person who draws the lines on the pitch.",
+      "Another name for the goalkeeper.",
+      "The person who commentates on the match.",
+    ],
+  },
+  {
+    id: "q7",
+    text: "When does a penalty shootout happen?",
+    answers: [
+      "If the teams are still level after extra time.",
+      "Whenever the referee feels like it.",
+      "At the ending of 90 minutes when both teams are level.",
+      "When a new player is brought in on extra-time.",
+    ],
+  },
+  {
+    id: "q8",
+    text: "When does the offside rule apply?",
+    answers: [
+      "When a player passes to another player closer to the other team's goal than the opposing defender.",
+      "When a player purposefully takes the ball outside of the play.",
+      "When player with the ball fouls the defender.",
+      "When a player kicks the ball to his goalkeeper from the mid-line",
+    ],
+  },
+  {
+    id: "q9",
+    text: "If an offensive player is fouled in the box the result is _____ kick.",
+    answers: ["Penalty.", "Direct.", "Corner.", "Free."],
+  },
+  {
+    id: "q10",
+    text: "A penalty kick is taken _______ yards away from the goal line.",
+    answers: ["12 yards.", "10 yards.", "15 yards.", "8 yards."],
+  },
+  {
+    id: "q11",
+    text: "Who has the highest number of Champions League goals?",
+    answers: [
+      "Cristiano Ronaldo - Portugal.",
+      "Lionel Messi - Argentina.",
+      "Robert Lewandowski - Poland.",
+      "Karim Benzema - France.",
+    ],
+  },
+];
