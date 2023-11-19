e389e0e1af1607d4556b644acb2f4a425a1cb47f
harshahossain <harshahossain@gmail.com>
2023-11-19 13:47:10 +0600

    Added Header and Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>    
+    <>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import mainLogo from "../assets/quiz-logo.png";
+//
+
+export default function Header() {
+  //
+  return (
+    <header>
+      <img src={mainLogo} alt="picture of a sheet and pens" />
+      <h1>Quiz</h1>
+    </header>
+  );
+}
